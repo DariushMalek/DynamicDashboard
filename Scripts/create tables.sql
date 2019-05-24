7d8b784e479c7ab9378f39f5364c76a1356f0225
Dariush Malek <nomalek@gmail.com>
2019-05-24 16:19:31 +0430

    delete migration

--- Scripts/create tables.sql
+++ Scripts/create tables.sql
@@ -6,5 +6,10 @@ create table dbo.MenuMaster
 (
 	Id int not null default (next value for dbo.MenuSeqId ),
 	MenuTitle nvarchar(50) not null,
-
+	UserRoleId int null,
+	ParentMenuId int null,
+	MenuURL nvarchar(100) null,
+	MenuAction nvarchar(100) null,
+	CreateDate datetime null,
+	UserId int null
 )
